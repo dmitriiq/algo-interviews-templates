98a99ecdc27cbb8740074a8722cf8947194c32d3
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-03-31 23:08:55 +0300

    O. Time limit

--- O/python/code.py
+++ O/python/code.py
@@ -1,16 +1,52 @@
 from typing import List
-
+from sortedcontainers import SortedDict
+from sortedcontainers import SortedList
 
 class Building:
     def __init__(self, need_capital, added_capital) -> None:
         self.need_capital = need_capital
         self.added_capital = added_capital
 
-
-
 def get_max_final_capital(buildings: List[Building], start_capital: int, max_buildings: int) -> int:
     # your code goes here
-    return 0
+    capital = start_capital
+    buildings.sort(reverse=True, key=lambda b: b.need_capital)
+    # print(buildings)
+
+    sortedDict = dict()
+    for b in buildings:
+        if not sortedDict.__contains__(b.need_capital):
+            sortedDict[b.need_capital] = SortedList()
+        for k in sortedDict:
+            if k >= b.need_capital:
+                sortedDict[k].add(b.added_capital)
+            # else:
+            #     break
+            # print('k, v: ', k, sortedDict[k])
+
+            # b.added_capital
+            # print('need_capital', b.need_capital, 'added', b.added_capital)
+
+    print(sortedDict)
+
+    while max_buildings > 0:
+        # print(max_buildings)
+        i = 0
+        for b in buildings:
+            # print('list: ', b.need_capital, i)
+            
+            if capital >= b.need_capital:
+                capital += b.added_capital
+                # print('capital', capital)
+                del buildings[i]
+                found = True
+                break
+            i += 1
+
+        max_buildings -= 1
+
+
+    return capital
 
 n, k = map(int, input().split())
 buildings = []
