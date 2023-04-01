1f42931ff98f3e2b2c5d3299945f4e28ab3fe12c
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-01 09:41:30 +0300

    O. sortedcontainers

--- O/python/code.py
+++ O/python/code.py
@@ -17,35 +17,49 @@ def get_max_final_capital(buildings: List[Building], start_capital: int, max_bui
     for b in buildings:
         if not sortedDict.__contains__(b.need_capital):
             sortedDict[b.need_capital] = SortedList()
-        for k in sortedDict:
-            if k >= b.need_capital:
-                sortedDict[k].add(b.added_capital)
-            # else:
-            #     break
-            # print('k, v: ', k, sortedDict[k])
+        sortedDict[b.need_capital].add(b.added_capital)
 
-            # b.added_capital
-            # print('need_capital', b.need_capital, 'added', b.added_capital)
+    need_capitals = []
+    for c in sortedDict:
+        need_capitals.append(c)
 
-    print(sortedDict)
+    it = 0
+    while it < len(need_capitals):
+        if need_capitals[it] > capital:
+            it += 1
+        else:
+            break
 
+    if it == len(need_capitals):
+        return capital
+    
     while max_buildings > 0:
-        # print(max_buildings)
-        i = 0
-        for b in buildings:
-            # print('list: ', b.need_capital, i)
-            
-            if capital >= b.need_capital:
-                capital += b.added_capital
-                # print('capital', capital)
-                del buildings[i]
-                found = True
+        it2 = it
+        max_it = it
+        max_added = sortedDict[need_capitals[it2]][-1]
+        # print('max_added', max_added)
+        while it2 < len(need_capitals):
+            # print('need_capitals[it2]', need_capitals[it2])#, sortedDict[need_capitals[it2]])
+            if sortedDict[need_capitals[it2]][-1] > max_added:
+                max_added = sortedDict[need_capitals[it2]][-1]
+                max_it = it2
+            it2 += 1
+            # print('len', len(need_capitals), 'it2', it2)
+        added_cap = max_added
+        del sortedDict[need_capitals[max_it]][-1]
+        if len(sortedDict[need_capitals[max_it]]) == 0:
+            del sortedDict[need_capitals[max_it]]
+            del need_capitals[max_it]
+        capital += added_cap
+        while it >= 0:
+            if need_capitals[it] <= capital:
+                it -= 1
+            else:
+                it += 1
                 break
-            i += 1
-
+        if it < 0:
+            break
         max_buildings -= 1
-
-
     return capital
 
 n, k = map(int, input().split())
