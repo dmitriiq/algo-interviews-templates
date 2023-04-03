7c670f4f1fa865b0fc6eb0f68efeeeba95ee0d07
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-03 20:47:05 +0300

    I. python success

--- I/python/code.py
+++ I/python/code.py
@@ -7,7 +7,24 @@ from node import Node
 #         self.next = next
 
 def Reverse(head: Node, left: int, right: int) -> Node:
-    # Your code
-    # “ヽ(´▽｀)ノ”
-    pass
-    
+    cnt = 0
+    arr = []
+    fakeHead = Node(0, head)
+    it = fakeHead
+
+    while it != None and cnt <= right:
+        # print(head.value)
+        if cnt >= left-1:
+            arr.append(it)
+            # print('arr: ', it.value)
+        it = it.next
+        cnt += 1
+
+    # print('it: ', it.value)
+    # print('arr[-1]: ', arr[-1].value)
+    arr[0].next = arr[-1]
+    for x in range(1, len(arr) - 1):
+        arr[x+1].next = arr[x]
+    arr[1].next = it
+
+    return fakeHead.next
