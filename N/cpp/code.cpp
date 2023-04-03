dc32e9a845da201e3ac514a6c338cc606287480f
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-03 20:26:43 +0300

    N. cpp compilation error

--- N/cpp/code.cpp
+++ N/cpp/code.cpp
@@ -1,17 +1,34 @@
 #include "solution.h"
+#include <vector>
 
-/** Comment it before submitting
-struct Node {
-    int val;
-    std::vector<Node*> neighbours;
-    Node(int val_) {
-        val = val_;
-        neighbours = {};
-    }
-};
-**/
+// Comment it before submitting
+// struct Node {
+//     int val;
+//     std::vector<Node*> neighbours;
+//     Node(int val_) {
+//         val = val_;
+//         neighbours = {};
+//     }
+// };
+
 
 Node* cloneGraph(Node* node) {
-    // Your code
-    // “ヽ(´▽｀)ノ”
+    static std::vector<Node*> visited;
+
+    for (int i = 0; i < visited.size(); ++i) {
+        if (visited[i]->val == node->val)
+            return visited[i];
+    }
+
+    Node* new_node = new Node(node->val);
+    visited.push_back(new_node);
+    for (int i = 0; i < node->neighbours.size(); ++i) {
+        cloneGraph(node->neighbours[i]);
+    }
+
+    return new_node;
 }
+
+// int main () {
+//     return 0;
+// }
