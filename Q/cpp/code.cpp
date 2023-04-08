cccc26120284e570c7619e3a8bca81eb718485fc
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-08 16:01:59 +0300

    Q. c++ success

--- Q/cpp/code.cpp
+++ Q/cpp/code.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
 struct Point {
-    int x;
-    int y;
+    long long x;
+    long long y;
 
-    Point(int x, int y): x(x), y(y) {
+    Point(long long x, long long y): x(x), y(y) {
     }
 };
 
 
 bool isOnOneLine(const vector<Point>& points) {
     // your code goes here
+    if (points.size() < 3)
+        return true;
+
+    if (points[1].x == points[0].x && points[1].y == points[0].y)
+        return isOnOneLine({points.begin() + 1, points.end()});
+
+    if (points[1].x != points[0].x) {
+        double a  = static_cast<double>(points[1].y - points[0].y) / (points[1].x - points[0].x);
+        double b = points[0].y - a * points[0].x;
+        // cout << "a: " << a << ", b: " << b << '\n';
+        double e = 1e-18;
+        // std::cout << e << '\n';
+        for (auto p : points) {
+            // if (p.y + p.y*e < a * p.x + b || p.y - p.y*e > a * p.x + b) {
+            //     std::cout << "py*e: " << p.y*e << ", ():" << a * p.x + b << '\n';
+
+            if (p.y != round(a * p.x + b)) {
+                // std::cout << "py: " << p.y << ", ():" << round(a * p.x + b) << '\n';
+                return false;
+            }
+        }
+        return true;
+    } else {
+        for (auto p : points) {
+            if (p.x != points[0].x)
+                return false;
+        }
+        return true;
+    }
+
+
     return false;
 }
 
@@ -22,7 +54,7 @@ int main() {
     cin >> n;
     vector<Point> points;
     for (int i = 0; i < n; i++) {
-        int x, y;
+        long long x, y;
         cin >> x >> y;
         points.push_back(Point(x, y));
     }
