f4372af481bd6678a57e782ccd4a2769122dcdb3
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-08 17:29:28 +0300

    R. python 4 test failed

--- R/python/code.py
+++ R/python/code.py
@@ -1,6 +1,21 @@
 def is_pasha_wins(n: int) -> bool:
-    # your code goes here
-    return False
+    if n < 2:
+        True
+
+    d = n - 1
+    Pasha_wins = False
+    print('pasha', Pasha_wins, 'd', d, 'n', n)
+    while d > 0:
+        if n % d != 0:
+            d -= 1
+        else:
+            print('d', d)
+            n = n - d
+            d = n - 1
+            Pasha_wins = not Pasha_wins
+            print('pasha', Pasha_wins, 'n', n)
+
+    return Pasha_wins
 
 n = int(input())
 if is_pasha_wins(n):
---
+++ R/test-py.bat
@@ -0,0 +1,7 @@
+echo 8 | python.exe .\python\code.py
+echo 3 | python.exe .\python\code.py
+echo 55 | python.exe .\python\code.py
+echo 10000 | python.exe .\python\code.py
+echo 9999 | python.exe .\python\code.py
+echo 1 | python.exe .\python\code.py
+echo 2 | python.exe .\python\code.py
