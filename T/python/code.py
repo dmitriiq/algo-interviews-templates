6910e10c542426f729f74aad8797faaa6a3d35aa
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-13 19:59:12 +0300

    T. python 29 time limit

--- T/python/code.py
+++ T/python/code.py
@@ -4,14 +4,22 @@ from typing import List
 def get_max_xor(numbers: List[int]) -> int:
     r = 0
 
-    i = 0
-    while i < len(numbers):
-        j = i + 1
-        while j < len(numbers):
-            # print(numbers[i] ^ numbers[j])
-            r = max(r, numbers[i] ^ numbers[j])
-            j += 1
-        i += 1
+    i = 0x80000000
+    while i > 0:
+        arr1 = []
+        arr2 = []
+        for n in numbers:
+            if n & i > 0:
+                arr1.append(n)
+            else:
+                arr2.append(n)
+        # print(i, arr1, arr2)
+        if len(arr1) > 0 and len(arr2) > 0:
+            for a in arr1:
+                for b in arr2:
+                    r = max(r, a^b)
+            i = 0
+        i = i >> 1
     return r
 
 n = int(input())
