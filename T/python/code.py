0288ab3d5387c0577a035d335f740592999e346d
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-13 19:05:53 +0300

    T. Python Time limit test 24

--- T/python/code.py
+++ T/python/code.py
@@ -2,8 +2,17 @@ from typing import List
 
 
 def get_max_xor(numbers: List[int]) -> int:
-    # your code goes here
-    return 0
+    r = 0
+
+    i = 0
+    while i < len(numbers):
+        j = i + 1
+        while j < len(numbers):
+            # print(numbers[i] ^ numbers[j])
+            r = max(r, numbers[i] ^ numbers[j])
+            j += 1
+        i += 1
+    return r
 
 n = int(input())
 numbers = list(map(int, input().split()))
---
+++ T/run_py.bat
@@ -0,0 +1,3 @@
+type ex1 | python python/code.py
+type ex2 | python python/code.py
+type ex3 | python python/code.py
