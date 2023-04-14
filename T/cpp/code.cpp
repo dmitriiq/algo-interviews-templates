c9a2249d2020c7ed1dd5c757efb16fc144ffa3fb
Dmitriy Baikov <dmitriy.baikov@gs-labs.ru>
2023-04-14 10:29:26 +0300

    T. cpp. success

--- T/cpp/code.cpp
+++ T/cpp/code.cpp
@@ -1,12 +1,108 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
+class Node {
+public:
+    Node() :
+        zero(nullptr),
+        one(nullptr),
+        val(0)
+    { }
+
+    std::unique_ptr<Node> zero;
+    std::unique_ptr<Node> one;
+    int val;
+
+    Node* insert(int bit) {
+        if (bit) {
+            if (one.get() == nullptr) {
+                one = make_unique<Node>();
+            }
+            return one.get();
+        } else {
+            if (zero.get() == nullptr) {
+                zero = make_unique<Node>();
+            }
+            return zero.get();
+        }
+    }
+
+    Node* itMaxXor(int bit) {
+        if (bit) {
+            if (zero.get() != nullptr) {
+                return zero.get();
+            } else {
+                return one.get();
+            }
+        } else {
+            if (one.get() != nullptr) {
+                return one.get();
+            } else {
+                return zero.get();
+            }
+        }
+    }
+};
 
 long long getMaxXOR(const vector<int>& list) {
-    // your code goes here
-    return 0;
+    int r = 0;
+
+    for (std::size_t i = 0x40000000; i > 0; i >>= 1) {
+        int len1 = 0;
+        int len2 = 0;
+        for (const auto n : list) {
+            if (n & i) {
+                len1++;
+            } else {
+                len2++;
+            }
+        }
+
+        if (len1 > 0 && len2 > 0) {
+            std::vector<int> v1;
+            std::vector<int> v2;
+
+            // std::cout << len1 << ' ' << len2 << '\n';
+            for (const auto n : list) {
+                if (n & i) {
+                    v1.push_back(n);
+                } else {
+                    v2.push_back(n);
+                }
+            }
+
+            Node tree;
+            
+            for (const auto n : v2) {
+                Node *it = &tree;
+
+                for (std::size_t j = i >> 1; j > 0 ; j >>= 1) {
+                    it = it->insert(n & j);
+                }
+
+                it->val = n;
+            }
+
+            for (const auto n : v1) {
+                Node *it = &tree;
+
+                for (std::size_t j = i >> 1; j > 0; j >>= 1) {
+                    it = it->itMaxXor(n & j);
+                }
+
+                r = std::max(r, n ^ it->val);
+            }
+
+            break;
+        }
+    }
+
+
+    return r;
 }
 
 vector<int> readList() {
---
+++ T/run_cpp.bat
@@ -0,0 +1,6 @@
+g++ .\cpp\code.cpp -o code.exe
+type ex1 | code.exe
+type ex2 | code.exe
+type ex3 | code.exe
+type ex4 | code.exe
+type ex5 | code.exe
